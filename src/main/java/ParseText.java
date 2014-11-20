3c2fe94dfb3e62f56c717947f2c1e65055b894c6
Viktoriya Oreshkina <vikus-jesus@mail.ru>
2014-11-20 17:25:01 +0600

    Initial commit

---
+++ src/main/java/ParseText.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ParseText {
+
+    public static void main(String[] args) {
+        String text = "London is a capital of Great Britain";
+
+        String[] word = new String[10];
+
+        char[] s = text.toCharArray();
+
+        for (int i = 0; i <= text.length(); i++) {
+            System.out.println(s[i]);
+        }
+
+
+        //   for (int i = 0; i++; )
+
+        ArrayList<String[]> letter = new ArrayList<String[]>();
+
+        for(int i = 0; i <= text.length(); i++){
+            letter.set(i) = 
+        }
+        int a = letter.size();
+        System.out.println(a);
+
+
+    }
+}
